09478318a636665f557e25075da0d5571e4e85c7
Dominic Magnifico <dominicmagnifico@gmail.com>
2017-06-26 17:46:05 -0700

    Remove old gulp tasks, start fresh with a lone webpack task.

---
+++ theme-name/gulp-tasks/webpack.js
@@ -0,0 +1,18 @@
+import gulp from 'gulp';
+import pump from 'pump';
+import webpack from 'webpack-stream';
+import source from 'vinyl-source-stream';
+
+// Webpack
+function processWebpack( src, conf, dest ) {
+	return gulp.src( src )
+	.pipe( webpack( require( conf ) ) )
+	.pipe( gulp.dest( dest ) )
+}
+
+gulp.task( 'webpack', () => {
+	const srcJs = './assets/js/**/*.js';
+	const conf = '../webpack.config.babel.js'
+	const fileDest = './dist/js';
+	processWebpack( srcJs, conf, fileDest );
+} );
