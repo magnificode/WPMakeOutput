67d4884d8ebdaf35d953ef89947239c82fe19030
Dominic Magnifico <dominic.magnifico@10up.com>
2017-06-29 18:35:45 -0600

    Combine cssnext and postcss processes into one file. Utilize cssnext to compile css.

---
+++ theme-name/gulp-tasks/cssnext.js
@@ -0,0 +1,43 @@
+import gulp from 'gulp';
+import postcss from 'gulp-postcss';
+import sassGlob from 'gulp-sass-glob';
+import pump from 'pump';
+
+/**
+ * Gulp task to run the sass task.
+ *
+ * @method
+ * @author Dominic Magnifico, 10up
+ * @example gulp sass
+ * @param   {Function} cb the pipe sequence that gulp should run.
+ * @returns {void}
+*/
+
+gulp.task( 'sass', function( cb ) {
+	const fileSrc = [
+			'./assets/css/admin/admin-style.css',
+			'./assets/css/frontend/editor-style.css',
+			'./assets/css/frontend/style.css',
+			'./assets/css/shared/shared-style.css'
+		];
+		const fileDest = './assets/css/src';
+		const cssNextOpts = {
+			features: {
+				autoprefixer: {
+					browsers: ['last 2 versions']
+				}
+			}
+		}
+		const taskOpts = [
+			require('postcss-import')(),
+			require('postcss-cssnext')( cssNextOpts )
+		];
+
+	pump( [
+		gulp.src( fileSrc ),
+		sassGlob(),
+		postcss( taskOpts ),
+		gulp.dest( fileDest )
+	], cb );
+
+} );
