282c700c022812ab600cef87b94143a2fc0ef738
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-01-02 11:30:23 -0700

    Whitespace fix.

--- theme-name/gulp-tasks/cssnext.js
+++ theme-name/gulp-tasks/cssnext.js
@@ -13,23 +13,23 @@ import pump from 'pump';
 */
 gulp.task( 'cssnext', ( cb ) => {
 	const fileSrc = [
-			'./assets/css/admin/admin-style.css',
-			'./assets/css/frontend/editor-style.css',
-			'./assets/css/frontend/style.css',
-			'./assets/css/shared/shared-style.css'
-		];
-		const fileDest = './assets/css/src';
-		const cssNextOpts = {
-			features: {
-				autoprefixer: {
-					browsers: ['last 2 versions']
-				}
+		'./assets/css/admin/admin-style.css',
+		'./assets/css/frontend/editor-style.css',
+		'./assets/css/frontend/style.css',
+		'./assets/css/shared/shared-style.css'
+	];
+	const fileDest = './assets/css/src';
+	const cssNextOpts = {
+		features: {
+			autoprefixer: {
+				browsers: ['last 2 versions']
 			}
 		}
-		const taskOpts = [
-			require( 'postcss-import' ),
-			require( 'postcss-cssnext' )( cssNextOpts )
-		];
+	};
+	const taskOpts = [
+		require( 'postcss-import' ),
+		require( 'postcss-cssnext' )( cssNextOpts )
+	];
 
 	pump( [
 		gulp.src( fileSrc ),
